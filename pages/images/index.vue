5cd0a0d2e93009611b45d846b94c851c8543561d
yacchiDAO <okamura@net.lab.uec.ac.jp>
2020-07-06 01:05:01 +0900

    bulmaのcardをあててみた

--- pages/images/index.vue
+++ pages/images/index.vue
@@ -6,7 +6,32 @@
     </div>
     <div class="columns is-multiline">
       <div v-for="image in images" :key="image.id" class="column is-3">
-        <img :src="image.image_url">
+
+        <div class="card">
+          <div class="card-image">
+            <figure class="image is-16by9">
+              <img :src="image.image_url">
+            </figure>
+          </div>
+          <div class="card-content">
+            <div class="media">
+              <div class="media-left">
+                <figure class="image is-128x128">
+                  <img :src="image.animation.image_url">
+                </figure>
+              </div>
+              <div class="media-content">
+                <p class="title is-4">{{image.line}}</p>
+                <p class="subtitle is-6">{{image.characters.map(function (character) {return character.name}).join('/')}}</p>
+              </div>
+            </div>
+
+            <div class="content">
+              {{image.description}}
+            </div>
+          </div>
+        </div>
+
       </div>
     </div>
   </section>
@@ -24,7 +49,6 @@
       searchImages: function() {
         this.$axios.$get(`/api/images?q=${this.query}`)
           .then(json => {
-            console.log(json[0])
             this.images = json
           })
           .catch(e => ({ error: e }))
